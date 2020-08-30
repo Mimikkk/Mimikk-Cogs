eff6d4c6bfe2ffed79c3b7459c84cf3bbee34ffe
Daniel Zdancewicz <34794358+Inwazzin@users.noreply.github.com>
2020-08-30 03:03:28 +0200

    Add files via upload

--- azurechan/data/consts.py
+++ azurechan/data/consts.py
@@ -4,6 +4,8 @@ class CONSTS(Enum):
     @skip
     class SQL(Enum):
         API_URL = "https://azurlane.koumakan.jp/w/index.php?"
+        WIKI_URL = "https://azurlane.koumakan.jp/"
+        WIKIPEDIA_URL = "https://wikipedia.org/wiki/"
 
         # Cursed SQL hacking
         SHIP_TABLE_ALL_FIELDS = 'ShipGroup,ShipID,Name,CNName,JPName,KRName,Rarity,Nationality,ConstructTime,Type,SubtypeRetro,Class,Artist,ArtistLink,ArtistPixiv,ArtistTwitter,Voiced,VA,Remodel,RemodelId,Image,ImageShipyardIcon,ImageChibi,ImageIcon,ImageBanner,ImageKai,ImageShipyardIconKai,ImageChibiKai,ImageIconKai,ImageBannerKai,HealthInitial,Armor,FireInitial,AAInitial,TorpInitial,AirInitial,ReloadInitial,EvadeInitial,ConsumptionInitial,Speed,Luck,AccInitial,ASWInitial,OxygenInitial,AmmoInitial,HealthMax,FireMax,AAMax,TorpMax,AirMax,ReloadMax,EvadeMax,ConsumptionMax,AccMax,ASWMax,OxygenMax,AmmoMax,HealthKai,ArmorKai,FireKai,AAKai,TorpKai,AirKai,ReloadKai,EvadeKai,ConsumptionKai,SpeedKai,ASWKai,AccKai,OxygenKai,AmmoKai,Health120,Fire120,AA120,Torp120,Air120,Reload120,Evade120,Consumption120,Acc120,ASW120,Oxygen120,Ammo120,HealthKai120,FireKai120,AAKai120,TorpKai120,AirKai120,ReloadKai120,EvadeKai120,ConsumptionKai120,AccKai120,ASWKai120,OxygenKai120,AmmoKai120,Eq1Type,Eq1EffInit,Eq1EffInitMax,Eq1EffInitKai,Eq2Type,Eq2EffInit,Eq2EffInitMax,Eq2EffInitKai,Eq3Type,Eq3EffInit,Eq3EffInitMax,Eq3EffInitKai,LB1,LB2,LB3'
@@ -43,14 +45,14 @@ class CONSTS(Enum):
             IMAGES = ("Image", "ImageShipyardIcon", "ImageChibi", "ImageIcon", "ImageBanner",
                       "ImageKai", "ImageShipyardIconKai", "ImageChibiKai", "ImageIconKai", "ImageBannerKai")
 
-            MENU_REACTIONS = u'❌🛶🛥⛴🚤🛳🎯🏵🪀ℹ🇷'
+            MENU_REACTIONS = u'❌🛶🛥⛴🚤🛳🎯🏵🪀🗺ℹ🇷'
             MENU_TYPES = ("Base Stats", "Max Stats", "Cognitive Max Stats",
                           "Retrofit Max Stats", "Cognitive Retrofit Max Stats")
 
         @skip
         class NORMAL(Enum):
             IMAGES = ("Image", "ImageShipyardIcon", "ImageChibi", "ImageIcon", "ImageBanner")
-            MENU_REACTIONS = u'❌🛶🛥⛴🎯🏵🪀ℹ'
+            MENU_REACTIONS = u'❌🛶🛥⛴🎯🏵🪀🗺ℹ'
             MENU_TYPES = ("Base Stats", "Max Stats", "Cognitive Max Stats")
 
         @skip
@@ -65,7 +67,7 @@ class CONSTS(Enum):
 
             TYPES = ("Health", "Armor", "Oil", "Acc", "Fire",
                      "Reload", "Anti-Air", "Torpedo", "Aviation", "Evasion",
-                     "Speed", "Luck", "ASW", "Oxygen", "Ammo", "Construction")
+                     "Speed", "Luck", "ASW", "Oxygen", "Ammo")
 
             # "Health", "Fire", "AA", "Torp", "Air", "Reload", "Evade", "Consumption", "Acc", "ASW", "Oxygen", "Ammo"
             # Not changing values are Luck, Speed, ConstructTime
