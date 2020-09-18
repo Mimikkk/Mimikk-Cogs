1795536377aac3ec469839adb4c00ab0d819bec2
Mimikkk <Inwazzin@gmail.com>
2020-09-18 19:37:16 +0200

    Data-Scrapping-Part

---
+++ azurechan/items_embed.py
@@ -0,0 +1,59 @@
+from .ship_embed import get_api_data, create_select_controls, get_image_url, cargo_query
+from .imports import *
+
+item_names: Dict[str, str] = dict()
+
+
+class ItemEmbed(object):
+    def __init__(self, name: str):
+        # Dummy Private Variables
+        self.__menu_reactions: Tuple[str, ...] = ()
+        self.__data: List[Dict[str, str]] = []
+        # Dummy Public Variables
+        self.controls: Dict[str, Callable] = {}
+        self.pages: List[discord.Embed] = []
+        self.variants_count = 0
+        # Pages
+
+        # Stats
+        # Stats Max
+        # Map Drop Location / Used by
+
+        # Initialization
+        self.__init_data(name)
+
+        self.__init_pages()
+        self.__init_controls()
+
+    def __init_data(self, name: str):
+        cargo_data = cargo_query(tables="equipment", fields=CONSTS.SQL.ITEM_TABLE_ALL_FIELDS.value,
+                                  where=f"equipment.Name='{item_names[name]}'", limit="10")
+        self.__data = [dict(map(lambda x: (x, str(item[x])), item)) for item in cargo_data.json]
+        self.__image_url: str = ""
+
+        item_rarity = {'1': u'🥇', '2': u'🥈', '3': u'🥉', '4': u'🏅', '5': u'🎖', '6': u'🏆'}
+        rarities = (item_rarity[str(self.__data[i]['Stars'])] for i in range(len(self.__data)))
+        self.__menu_reactions = (u'❌', *rarities, u'🗺', u'ℹ')
+        self.__image_url = get_image_url(self.__data[0]['Image'])
+
+    def __init_pages(self):
+        self.__page_stats()
+        self.__page_drops()
+        self.__page_info()
+
+    def __page_constructor(self):
+        pass
+
+    def __page_stats(self):
+        for i in range(len(self.__data)):   # hopefully they're in oreder
+            pass
+        pass
+
+    def __page_drops(self):
+        pass
+
+    def __page_info(self):
+        pass
+
+    def __init_controls(self):
+        self.controls = create_select_controls(self.__menu_reactions)
---
+++ azurechan/utils.py
@@ -0,0 +1,47 @@
+from .imports import *
+
+
+def create_select_controls(reactions):
+    """Creates Menu's Reaction Based Navigation"""
+
+    async def select_page(ctx, pages, controls, message, page, timeout, emoji):
+        # if can manage messages remove the react
+        if message.channel.permissions_for(ctx.me).manage_messages:
+            with contextlib.suppress(discord.NotFound): await message.remove_reaction(emoji, ctx.author)
+        return await menus.menu(ctx, pages, controls, message=message, page=page, timeout=timeout)
+
+    def wrap(func, num: int):
+        async def wrapper(*args):
+            args = list(args)
+            args[4] = num
+            return await func(*args)
+
+        return wrapper
+
+    # could use old good '< x >' keys
+    # self.controls = menus.DEFAULT_CONTROLS
+    _select: Callable[[int], Callable] = lambda num: wrap(select_page, num)
+    return {emoji: menus.close_menu if i == -1 else _select(i) for (i, emoji) in enumerate(reactions, -1)}
+
+def cargo_query(tables: str = "", fields: str = "", where: str = "", limit: str = "50", output_format: str = "json",
+                api_url: str = "https://azurlane.koumakan.jp/w/index.php?",
+                offset: str = "0"):
+    return requests.get(url=api_url + "title=Special:CargoExport"
+                                      f"&tables={tables}&fields={fields}&where={where}"
+                                      f"&offset={offset}&limit={limit}&format={output_format}")
+
+def get_api_data(action: str, api_url: str = "https://azurlane.koumakan.jp/w/api.php") -> requests.Response:
+    return requests.get(url=api_url + action)
+
+def get_image_url(image_name: str, api_url: str = "https://azurlane.koumakan.jp") -> str:
+    return f'{api_url}/Special:Redirect/file/{parse.quote(image_name.replace(" ", "_"))}?width=800'
+
+
+def get_name_url(name: str, api_url: str = "https://azurlane.koumakan.jp") -> str:
+    return f'{api_url}/{parse.quote(name.replace(" ", "_"))}'
+
+def get_emoji(stat: str) -> str:
+    return CONSTS.SHIP.STATS.EMOJI.value[stat]
+
+def embed_url(name: str, link: str) -> str:
+    return f"[{name}]({link.replace(' ', '_')})"
