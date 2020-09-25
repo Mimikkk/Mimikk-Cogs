b52fe5a1d9b3652906c39dd28d3c7317e00787d4
Mimikkk <Inwazzin@gmail.com>
2020-09-25 12:56:03 +0200

    Refactoring

--- azurechan/data/consts.py
+++ azurechan/data/consts.py
@@ -1,19 +1,33 @@
 from aenum import Enum, skip
 
+
 class CONSTS(Enum):
+    EMOJI = {"Health": u'❤', "PlaneHP": u'💞', "Armor": u'🛡', "Consumption": u'🛢', "Acc": u'🎯', "Fire": u'🔥',
+             "Reload": u'♻', "AA": u'📡', "Torp": u'🥢', "Air": u'🛩', "Evade": u'👥',
+             "Speed": u'⏩', "Luck": u'🎱', "ASW": u'🛥', "Oxygen": u'☁', "Ammo": u'🎹',
+             "ConstructTime": u'🛠', "Firepower": u'🔥', "Aviation": u'🛩', "RoF": u'♻',
+             "Accuracy": u'🎯', "Torpedo": u'🥢', 'Coin': u'📀', "Medal": u'🎖', 'Tech': u'✨',
+             "Oil": u'🛢', "Evasion": u'👥', "Anti-Air": u'📡', 'Specialized Core': u'🔬',
+             'Notes': u'📄', 'DropLocation': "🗺", "Damage": u'💥', 'Bombs': u'💣'}
+
     @skip
     class SQL(Enum):
         API_URL = "https://azurlane.koumakan.jp/w/index.php?"
         WIKI_URL = "https://azurlane.koumakan.jp/"
         WIKIPEDIA_URL = "https://wikipedia.org/wiki/"
 
-        # Cursed SQL hacking
+        # Cursed WikiApi SQL
         SHIP_TABLE_ALL_FIELDS = 'ShipGroup,ShipID,Name,CNName,JPName,KRName,Rarity,Nationality,ConstructTime,Type,SubtypeRetro,Class,Artist,ArtistLink,ArtistPixiv,ArtistTwitter,Voiced,VA,Remodel,RemodelId,Image,ImageShipyardIcon,ImageChibi,ImageIcon,ImageBanner,ImageKai,ImageShipyardIconKai,ImageChibiKai,ImageIconKai,ImageBannerKai,HealthInitial,Armor,FireInitial,AAInitial,TorpInitial,AirInitial,ReloadInitial,EvadeInitial,ConsumptionInitial,Speed,Luck,AccInitial,ASWInitial,OxygenInitial,AmmoInitial,HealthMax,FireMax,AAMax,TorpMax,AirMax,ReloadMax,EvadeMax,ConsumptionMax,AccMax,ASWMax,OxygenMax,AmmoMax,HealthKai,ArmorKai,FireKai,AAKai,TorpKai,AirKai,ReloadKai,EvadeKai,ConsumptionKai,SpeedKai,ASWKai,AccKai,OxygenKai,AmmoKai,Health120,Fire120,AA120,Torp120,Air120,Reload120,Evade120,Consumption120,Acc120,ASW120,Oxygen120,Ammo120,HealthKai120,FireKai120,AAKai120,TorpKai120,AirKai120,ReloadKai120,EvadeKai120,ConsumptionKai120,AccKai120,ASWKai120,OxygenKai120,AmmoKai120,Eq1Type,Eq1EffInit,Eq1EffInitMax,Eq1EffInitKai,Eq2Type,Eq2EffInit,Eq2EffInitMax,Eq2EffInitKai,Eq3Type,Eq3EffInit,Eq3EffInitMax,Eq3EffInitKai,LB1,LB2,LB3'
         ITEM_TABLE_ALL_FIELDS = 'Name,Image,Type,Stars,Nationality,Tech,Health,HealthMax,Torpedo,TorpMax,Firepower,FPMax,Aviation,AvMax,Evasion,EvasionMax,PlaneHP,PlaneHPMax,Reload,ReloadMax,ASW,ASWMax,Oxygen,OxygenMax,AA,AAMax,Luck,LuckMax,Acc,AccMax,Spd,SpdMax,Damage,DamageMax,RoF,RoFMax,Number,Spread,Angle,WepRange,Shells,Salvoes,Characteristic,PingFreq,VolleyTime,Coef,Ammo,AAGun1,AAGun2,Bombs1,Bombs2,DD,DDNote,CL,CLNote,CA,CANote,CB,CBNote,BM,BMNote,BB,BBNote,BC,BCNote,BBV,BBVNote,CV,CVNote,CVL,CVLNote,AR,ARNote,SS,SSNote,SSV,SSVNote,DropLocation,Notes'
 
-
     @skip
     class SHIP(Enum):
+        CLASSES = {"DD": "Destroyer", "CL": "Light Cruiser", "CA": "Heavy Cruiser",
+                   "CB": "Large Cruiser", "BM": "Monitor", "BB": "Battleship",
+                   "BC": "Battlecruiser", "BBV": "Aviation Battleship", "CV": "Aircraft Carrier",
+                   "CVL": "Light Aircraft Carrier", "AR": "Repair Ship", "SS": "Submarine",
+                   "SSV": "Submarine Carrier", "AE": "Munition"}
+
         @skip
         class RARITY(Enum):
             """U0 N1 R2 E3 SR4 P4 UR5 D5"""
@@ -52,12 +66,6 @@ class CONSTS(Enum):
         @skip
         class STATS(Enum):
             # They really can't decide what text to use..
-            EMOJI = {"Health": u'❤', "Armor": u'🛡', "Consumption": u'🛢', "Acc": u'🎯', "Fire": u'🔥',
-                     "Reload": u'♻', "AA": u'📡', "Torp": u'🥢', "Air": u'🛩', "Evade": u'👥',
-                     "Speed": u'⏩', "Luck": u'🎱', "ASW": u'🛥', "Oxygen": u'☁', "Ammo": u'🎹',
-                     "ConstructTime": u'🛠', "Firepower": u'🔥', "Aviation": u'🛩', "RoF": u'♻',
-                     "Accuracy": u'🎯', "Torpedo": u'🥢', 'Coin': u'📀', "Medal": u'🎖', 'Tech': u'✨',
-                     "Oil": u'🛢', "Evasion": u'👥', "Anti-Air": u'📡', 'Specialized Core': u'🔬'}
 
             TYPES = ("Health", "Armor", "Oil", "Acc", "Fire",
                      "Reload", "Anti-Air", "Torpedo", "Aviation", "Evasion",
@@ -69,3 +77,17 @@ class CONSTS(Enum):
                       "Fire": "Fire", "Reload": "Reload", "Anti-Air": "AA", "Torpedo": "Torp",
                       "Aviation": "Air", "Evasion": "Evade", "Speed": "Speed", "Luck": "Luck",
                       "ASW": "ASW", "Ammo": "Ammo", "Oxygen": "Oxygen", "Construction": "ConstructTime"}
+
+    @skip
+    class ITEM(Enum):
+        BASE_STATS = ("Health", "Torpedo", "Firepower", "Aviation",
+                      "Evasion", "PlaneHP", "Reload", "ASW",
+                      "Oxygen", "AA", "Luck", "Acc", "Spd")
+
+        MAX_STATS = ("HealthMax", "TorpMax", "FPMax", "AvMax",
+                     "EvasionMax", "PlaneHPMax", "ReloadMax", "ASWMax",
+                     "OxygenMax", "AAMax", "LuckMax", "AccMax", "SpdMax", "RoFMax",)
+
+        RARITY_EMOJI = {'1': u'🥉', '2': u'🥈', '3': u'🥇', '4': u'🏅', '5': u'🎖', '6': u'🏆'}
+        STARS = {'1': u'N-⭐', '2': u'N-⭐⭐', '3': u'R-⭐⭐⭐', '4': u'E-⭐⭐⭐⭐', '5': u'SR-⭐⭐⭐⭐⭐', '6': u'UR-⭐⭐⭐⭐⭐⭐'}
+        COLOR = {'1': 0xCCCCCC, '2': 0xCCCCCC, '3': 0x41D7FF, '4': 0xCC7BFF, '5': 0xFDC637, '6': 0xBD4000}
--- azurechan/items_embed.py
+++ azurechan/items_embed.py
@@ -1,4 +1,4 @@
-from .utils import get_api_data, create_select_controls, get_image_url, cargo_query, get_name_url, embed_url
+from .utils import get_api_data, create_select_controls, get_image_url, cargo_query, get_name_url, embed_url, get_emoji
 from .imports import *
 
 item_names: Dict[str, str] = dict()
@@ -31,24 +31,18 @@ class ItemEmbed(object):
         for dict_ in cargo_data.json():
             self.__data.append(dict(map(lambda x: (x, str(dict_[x])), dict_)))
 
-        item_rarity = {'1': u'🥉', '2': u'🥈', '3': u'🥇', '4': u'🏅', '5': u'🎖', '6': u'🏆'}
-        item_stars = {'1': u'N-⭐', '2': u'N-⭐⭐', '3': u'R-⭐⭐⭐',
-                      '4': u'E-⭐⭐⭐⭐', '5': u'SR-⭐⭐⭐⭐⭐', '6': u'UR-⭐⭐⭐⭐⭐⭐'}
-        item_colors = {'1': 0xCCCCCC, '2': 0xCCCCCC, '3': 0x41D7FF,
-                       '4': 0xCC7BFF, '5': 0xFDC637, '6': 0xBD4000}
-
         self.__name = self.__data[0]['Name'].replace("&quot;", "\"")
-        item_misc_data = get_api_data(
-            f"?action=parse&page={parse.quote(self.__name.replace(' ', '_'))}&prop=wikitext&format=json")
+        item_misc_data = \
+            get_api_data(f"?action=parse&page={parse.quote(self.__name.replace(' ', '_'))}&prop=wikitext&format=json")
 
         item_misc_data = dict(re.findall(r' \| (AE.*) = (.+)', item_misc_data.json()['parse']['wikitext']['*']))
         for dict_ in self.__data:
             dict_['AE'] = '1' if item_misc_data.get('AE') else ''
             dict_['AENote'] = note if (note := item_misc_data.get('AENotes')) else ''
 
-        rarities = (item_rarity[str(self.__data[i]['Stars'])] for i in range(len(self.__data)))
-        self.__stars = tuple((item_stars[str(self.__data[i]['Stars'])] for i in range(len(self.__data))))
-        self.__colors = tuple((item_colors[str(self.__data[i]['Stars'])] for i in range(len(self.__data))))
+        rarities = (CONSTS.ITEM.RARITY_EMOJI.value[self.__data[i]['Stars']] for i in range(len(self.__data)))
+        self.__stars = tuple((CONSTS.ITEM.STARS.value[self.__data[i]['Stars']] for i in range(len(self.__data))))
+        self.__colors = tuple((CONSTS.ITEM.COLOR.value[self.__data[i]['Stars']] for i in range(len(self.__data))))
         self.__menu_reactions = (u'❌', *rarities, u'ℹ')
         self.__image_url = get_image_url(self.__data[0]['Image'])
         self.__factor_notes()
@@ -68,9 +62,9 @@ class ItemEmbed(object):
         self.__page_stats()
         self.__page_usability()
 
-    def __page_constructor(self, author_desc: str, color: int, footer_desc: str):
+    def __page_constructor(self, color: int, footer_desc: str):
         embed = (discord.Embed(color=color)
-                 .set_author(name=author_desc, icon_url=self.__image_url)
+                 .set_author(name=f"{self.__data[0]['Nationality']} {self.__data[0]['Type']}", icon_url=self.__image_url)
                  .set_thumbnail(url=self.__image_url)
                  .set_footer(text=f"Page {len(self.pages) + 1} : {footer_desc}", icon_url=self.__image_url))
         embed.title = self.__name
@@ -79,35 +73,18 @@ class ItemEmbed(object):
 
     def __page_stats(self):
 
-        emoji = {"PlaneHP": u'💞', "Health": u'❤', "Acc": u'🎯', "Damage": u'💥',
-                 "Reload": u'♻', "AA": u'📡', "Torp": u'🥢', "Air": u'🛩',
-                 "Speed": u'⏩', "Luck": u'🎱', "ASW": u'🛥', "Oxygen": u'☁', "Ammo": u'🎹',
-                 "ConstructTime": u'🛠', "Firepower": u'🔥', "Aviation": u'🛩', "RoF": u'♻',
-                 "Accuracy": u'🎯', "Torpedo": u'🥢', 'Tech': u'✨', "Evasion": u'👥',
-                 'Notes': u'📄', 'DropLocation': "🗺", 'Bombs': u'💣'}
-
-        stats = ("Health", "Torpedo", "Firepower", "Aviation",
-                 "Evasion", "PlaneHP", "Reload", "ASW",
-                 "Oxygen", "AA", "Luck", "Acc", "Spd")
-
-        max_stats = ("HealthMax", "TorpMax", "FPMax", "AvMax",
-                     "EvasionMax", "PlaneHPMax", "ReloadMax", "ASWMax",
-                     "OxygenMax", "AAMax", "LuckMax", "AccMax", "SpdMax", "RoFMax",)
-
         for i in range(len(self.__data)):  # hopefully they're in-order
-            embed = self.__page_constructor(f"{self.__data[0]['Nationality']} {self.__data[i]['Type']}",
-                                            self.__colors[i],
-                                            f"Stats & Max Stats, {self.__stars[i]}")
+            embed = self.__page_constructor(self.__colors[i], f"Stats & Max Stats, {self.__stars[i]}")
 
             has_stats: bool = False
-            for (norm_, max_) in zip(stats, max_stats):
+            for (norm_, max_) in zip(CONSTS.ITEM.BASE_STATS.value, CONSTS.ITEM.MAX_STATS.value):
                 if self.__data[i][norm_] in ' 0' and self.__data[i][max_] in ' 0': continue
 
                 if not has_stats:
                     has_stats = True
                     embed.add_field(name="Stat Changes:", value='\u200b', inline=False)
 
-                embed.add_field(name=f"{emoji[norm_]}: {self.__data[i][norm_]}"
+                embed.add_field(name=f"{get_emoji(norm_)}: {self.__data[i][norm_]}"
                                      f"{' → ' + self.__data[i][max_] if self.__data[i][max_] else ''}",
                                 value="\u200b", inline=True)
 
@@ -115,35 +92,35 @@ class ItemEmbed(object):
             if self.__data[i]['Salvoes']:
                 salvoes = self.__data[i]['Salvoes']
                 shells = self.__data[i]['Shells']
-                specialized[f"{emoji['Torp']} Volleys:"] = f"**{salvoes} x {shells}** shells"
+                specialized[f"{get_emoji('Torp')} Volleys:"] = f"**{salvoes} x {shells}** shells"
 
             if self.__data[i]['Characteristic']:
                 ammo = self.__data[i]["Ammo"] if self.__data[i]["Ammo"] else "Normal"
                 character = self.__data[i]["Characteristic"] if self.__data[i]["Characteristic"] else "Normal"
-                specialized[f"{emoji['Ammo']} Ammunition:"] = f"**{ammo}** Ammo with **{character}** Characteristic"
+                specialized[f"{get_emoji('Ammo')} Ammunition:"] = f"**{ammo}** Ammo with **{character}** Characteristic"
 
             if self.__data[i]['Angle']:
                 angle = self.__data[i]['Angle']
                 spread = self.__data[i]['Spread']
                 range_ = self.__data[i]['WepRange']
-                specialized[f"{emoji['Acc']} Angle and Spread:"] = (
+                specialized[f"{get_emoji('Acc')} Angle and Spread:"] = (
                         f"**{angle}°**"
                         + f" ± **{spread}°**" * bool(spread)
                         + f" with the range of **{range_}**" * bool(range_))
 
             if self.__data[i]['PingFreq']:
                 ping_freq = self.__data[i]['PingFreq']
-                specialized[f"{emoji['AA']} Radar:"] = f"**{ping_freq}s** per swap"
+                specialized[f"{get_emoji('AA')} Radar:"] = f"**{ping_freq}s** per swap"
 
             if self.__data[i]['AAGun1']:
                 aa_gun1 = self.__data[i]['AAGun1']
                 aa_gun2 = self.__data[i]['AAGun2']
-                specialized[f"{emoji['AA']} Anti-Air Guns:"] = f"**{aa_gun1}**" + f" and **{aa_gun2}**" * bool(aa_gun2)
+                specialized[f"{get_emoji('AA')} Anti-Air Guns:"] = f"**{aa_gun1}**" + f" and **{aa_gun2}**" * bool(aa_gun2)
 
             if self.__data[i]['Bombs1']:
                 bombs1 = self.__data[i]['Bombs1']
                 bombs2 = self.__data[i]['Bombs2']
-                specialized[f"{emoji['Bombs']} Bombs:"] = f"**{bombs1}**" + f" and **{bombs2}**" * bool(bombs2)
+                specialized[f"{get_emoji('Bombs')} Bombs:"] = f"**{bombs1}**" + f" and **{bombs2}**" * bool(bombs2)
 
             if self.__data[i]['Damage']:
                 dmg = self.__data[i]['Damage']
@@ -151,14 +128,10 @@ class ItemEmbed(object):
                 rof = self.__data[i]['RoF']
                 max_rof = self.__data[i]['RoFMax']
                 volley_time = self.__data[i]['VolleyTime']
-                salvoes = int(self.__data[i]['Salvoes']) if self.__data[i]['Salvoes'] else 0
-                shells = int(self.__data[i]['Shells']) if self.__data[i]['Shells'] else 0
-                projectiles = max(salvoes, shells, salvoes * shells)
                 coef = self.__data[i]['Coef']
 
-                specialized[f"{emoji['Damage']} Attack:"] = (
+                specialized[f"{get_emoji('Damage')} Attack:"] = (
                         f"**{dmg} → {max_dmg}**"
-                        + f" **x {projectiles}**" * bool(projectiles > 1)
                         + f" with rate of fire of **{rof}s → {max_rof}**s per volley" * bool(rof)
                         + f" at **{coef}%** Efficiency" * bool(coef)
                         + f" and action time of **{volley_time}s**" * bool(volley_time))
@@ -167,37 +140,26 @@ class ItemEmbed(object):
                 embed.add_field(name=stat_name, value=stat_format, inline=False)
 
             if self.__data[i]['Notes']:
-                embed.add_field(name=f"{emoji['Notes']} Note:", value=self.__data[i]['Notes'], inline=False)
+                embed.add_field(name=f"{get_emoji('Notes')} Note:", value=self.__data[i]['Notes'], inline=False)
 
             drop_info = self.__data[i]['DropLocation'] if self.__data[i]['DropLocation'] else "Unknown"
-            embed.add_field(name=f"{emoji['DropLocation']} Acquired:", value=drop_info, inline=False)
+            embed.add_field(name=f"{get_emoji('DropLocation')} Acquired:", value=drop_info, inline=False)
 
             self.pages.append(embed)
 
     def __page_usability(self):
-        def format_class(class_type: str):
-            return (f"{ship_classes[class_type]}-{class_type}",
-                    f"{use_classes[class_type]} {self.__data[0][class_type + 'Note']}")
-
-        embed = self.__page_constructor(f"{self.__data[0]['Nationality']} {self.__data[0]['Type']}",
-                                        self.__colors[-1],
-                                        f"Usability")
-
-        ship_classes = {"DD": "Destroyer", "CL": "Light Cruiser", "CA": "Heavy Cruiser",
-                        "CB": "Large Cruiser", "BM": "Monitor", "BB": "Battleship",
-                        "BC": "Battlecruiser", "BBV": "Aviation Battleship", "CV": "Aircraft Carrier",
-                        "CVL": "Light Aircraft Carrier", "AR": "Repair Ship", "SS": "Submarine",
-                        "SSV": "Submarine Carrier", "AE": "Munition"}
+        embed = self.__page_constructor(self.__colors[-1], "Usability")
 
         use_classes = {}
-        for class_ in ship_classes:
-            use_classes[class_] = (u'✅' if self.__data[0][class_] == '1'
-                                   else (u'☑' if self.__data[0][class_] == '2' else u'❌'))
+        for class_ in CONSTS.SHIP.CLASSES.value:
+            use_classes[class_] = \
+                (u'✅' if self.__data[0][class_] == '1' else (u'☑' if self.__data[0][class_] == '2' else u'❌'))
 
         embed.add_field(name="Used By:", value="\u200b", inline=False)
         for class_ in use_classes:
-            name, value = format_class(class_)
-            embed.add_field(name=name, value=value, inline=False)
+            embed.add_field(name=f"{CONSTS.SHIP.CLASSES.value[class_]}-{class_}",
+                            value=f"{use_classes[class_]} {self.__data[0][class_ + 'Note']}",
+                            inline=False)
         self.pages.append(embed)
 
     def __init_controls(self):
